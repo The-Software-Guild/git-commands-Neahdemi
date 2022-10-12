c90cf681d0b3b2c3c4c3e533c226de14919b253b
Neah Demi <Neahdemi98@gmail.com>
2022-10-12 09:35:15 +0100

    NetBeans

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/** 
+*@author Neah Demi Banberry-Duhaney
+ *email: neahdemi98@gmail.com
+ *date: 11/10/2022
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.neahdemi.helloworld;
+
+/**
+ *
+ * @author neahbanberry-duhaney
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Variables/src/main/java/com/neahdemi/variables/Variables.java
@@ -0,0 +1,26 @@
+package com.neahdemi.variables;
+
+/**
+ *
+ * @author neahbanberry-duhaney
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+   
+    // data_type variable_name;
+    
+        // decalre an int called counter
+        int counter;
+        //now assign the number 7 to counter
+        counter = 7;
+        
+        //decalre a boolean called isDone
+        boolean isDone;
+        //now assign false to isDone
+        isDone = false;
+        
+        
+        
+    }
+}
