1d3b53c4e21b302947a116b14e9251677517eea8
Neah Demi <Neahdemi98@gmail.com>
2022-10-14 13:16:02 +0100

    projectGutenberg

--- PracticeProgramming/src/main/java/com/sg/foundations/basics/ProjectGutenberg.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/ProjectGutenberg.java
@@ -14,7 +14,8 @@ public class ProjectGutenberg {
         System.out.println("We are like a modern Gutenberg,");
         System.out.println("printing vast amounts to the waiting console with ease.");
         
-        //try using System.out.print insetad 
+        //try using System.out.print instead, are you can see when ypu run file,
+        //all the code is on one line 
         System.out.print("Did you know that in 1440 (or thereabouts),");
         System.out.print("Johannes Gutenberg invented the printing press?");
         System.out.print("He started out as a goldsmith!");
